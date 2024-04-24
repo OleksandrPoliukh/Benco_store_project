287d45863389dd35811ae16ed7c6e323dfd2490e
Oleksandr Poliukh <alexpolyuh95@gmail.com>
2024-04-24 16:19:07 +0300

    Add popup for Auth without functions

--- js/main.js
+++ js/main.js
@@ -93,13 +93,13 @@ const sec = document.getElementById("sec");
 let today = new Date();
 let monday = new Date();
 
-if(today.getDay()){
+if (today.getDay()) {
     monday.setDate(today.getDate() + 8 - today.getDay())
 } else {
     monday.setDate(today.getDate() + 1)
 }
 
-let midnight = today.getHours()*3600000 + today.getMinutes()*60000 + today.getSeconds()*1000 + today.getMilliseconds();
+let midnight = today.getHours() * 3600000 + today.getMinutes() * 60000 + today.getSeconds() * 1000 + today.getMilliseconds();
 let newWeek = monday - midnight;
 
 const circleD = document.querySelector('.progress-ring__circle-d');
@@ -162,11 +162,11 @@ function countdownTimer() {
     min.innerHTML = m < 10 ? "0" + m : m;
     sec.innerHTML = s < 10 ? "0" + s : s;
 
-    const daysPercent = ( ( 100 / 7 ) * d)
-    const hoursPercent = ( ( 100 / 24 ) * h)
-    const minutesPercent = ( ( 100 / 60 ) * m)
-    const secondsPercent = ( ( 100 / 60 ) * s)
-    
+    const daysPercent = ((100 / 7) * d)
+    const hoursPercent = ((100 / 24) * h)
+    const minutesPercent = ((100 / 60) * m)
+    const secondsPercent = ((100 / 60) * s)
+
     setProgressD(daysPercent);
     setProgressH(hoursPercent);
     setProgressM(minutesPercent);
@@ -177,16 +177,15 @@ setInterval(countdownTimer, 1000);
 
 
 
-
 // TG email sending
 
 const TOKEN = "6958554100:AAFyt3gPgTrfu5beUay1n4MRBBie8NHnjEY";
 const CHAT_ID = "-1002001883773";
-const URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
+const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
-document.getElementById('tg').addEventListener('submit', function(e) {
+document.getElementById('tg').addEventListener('submit', function (e) {
     e.preventDefault();
-    
+
     let message = `<b>New consumer:</b>\n`;
     message += `${this.email.value}`
 
@@ -195,14 +194,140 @@ document.getElementById('tg').addEventListener('submit', function(e) {
         parse_mode: 'html',
         text: message
     })
-    .then((res) => {
-        this.email.value = "";
-        alert("Your email was successfully sent. Wait for callback from manager.")
-    })
-    .catch((err) => {
-        console.warn(err);
-    })
-    .finally(() => {
-        console.log('Finish')
-    })
+        .then((res) => {
+            this.email.value = "";
+            alert("Your email was successfully sent. Wait for callback from manager.")
+        })
+        .catch((err) => {
+            console.warn(err);
+        })
+        .finally(() => {
+            console.log('Finish')
+        })
 });
+
+
+
+// popup
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index];
+        popupLink.addEventListener('click', function (e) {
+            const popupName = popupLink.getAttribute('href').replace('#', '');
+            const currentPopup = document.getElementById(popupName);
+            popupOpen(currentPopup);
+            e.preventDefault();
+        });
+    }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+        });
+    }
+}
+
+function popupOpen(currentPopup) {
+    if (currentPopup && unlock) {
+        const popupActive = document.querySelector('.popup.open');
+        if (popupActive) {
+            popupClose(popupActive, false);
+        } else {
+            bodyLock();
+        }
+        currentPopup.classList.add('open');
+        currentPopup.addEventListener('click', function (e) {
+            if (!e.target.closest('.popup__content')) {
+                popupClose(e.target.closest('.popup'));
+            }
+        });
+    }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+        popupActive.classList.remove('open');
+        if (doUnlock) {
+            bodyUnlock();
+        }
+    }
+}
+
+function bodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+    if (lockPadding.length > 0) {
+        for (let index = 0; index < array.lockPadding; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = lockPaddingValue;
+        }
+    }
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add('lock');
+
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;
+    }, timeout);
+}
+
+function bodyUnlock() {
+    setTimeout(function () {
+        if (lockPadding.length > 0) {
+            for (let index = 0; index < lockPadding.length; index++) {
+                const el = lockPadding[index];
+                el.style.paddingRight = '0px';
+            }
+        }
+        body.style.paddingRight = '0px';
+        body.classList.remove('lock');
+    }, timeout);
+
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;
+    }, timeout);
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.which === 27) {
+        const popupActive = document.querySelector('.popup.open');
+        popupClose(popupActive);
+    }
+});
+
+// polyphiles for popup
+(function () {
+    if (!Element.prototype.closest) {
+        Element.prototype.closest = function (css) {
+            var node = this;
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+            return null;
+        };
+    }
+})();
+(function () {
+    if (!Element.prototype.matches) {
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+        Element.prototype.webkitMatchesSelector ||
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMathesSelector;
+    }
+})();
