d75108c87a93d0969696792cb21fc094aa4fbed3
Oleksandr Poliukh <alexpolyuh95@gmail.com>
2024-05-21 16:27:41 +0300

    Set google btn for auth

--- js/main.js
+++ js/main.js
@@ -400,11 +400,11 @@ function clearValidation() {
 const baseUrl = 'https://662a665b67df268010a3c347.mockapi.io/api/v1/benco_users';
 const registrationForm = document.getElementById("rg-form");
 const signInForm = document.getElementById("snin-form");
-const signOut = document.getElementById("signOut-popup__btn");
+const simpleSignOut = document.getElementById("signOut-popup__btn");
 
 registrationForm.addEventListener('submit', sendRegistrationForm);
 signInForm.addEventListener('submit', authorization);
-signOut.addEventListener('click', signingOut);
+simpleSignOut.addEventListener('click', signingOut);
 
 function signingOut() {
     alert("You are successfully Signed Out")
@@ -506,5 +506,28 @@ async function sendRegistrationForm(event) {
     }
 }
 
+//google auth
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    document.getElementById("myAcc-btn").style.display = "none";
+    document.getElementById("signOut-popup__btn").style.display = "none";
+    document.getElementById("snOut-btn").style.display = "block";
+    document.getElementById("signOut-popup__google-btn").style.display = "block";
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert('User signed out.');
+        document.getElementById("myAcc-btn").style.display = "block";
+        document.getElementById("signOut-popup__btn").style.display = "block";
+        document.getElementById("snOut-btn").style.display = "none";
+        document.getElementById("signOut-popup__google-btn").style.display = "none";
+    });
+}
 
